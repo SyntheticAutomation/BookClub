7401993b3e9a301bec82b5730d2656985c12b4d9
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-04 22:47:52 -0700

    Adjusts first test for user_story_6

--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -3,21 +3,15 @@ require 'rails_helper'
 describe 'A visitor to our web app' do
   describe 'when I visit the books index page (User Story 6)' do
     before(:each) do
-    book_1 = Book.create(title: Faker::Book.title, authors: Faker::Book.author, pages: Faker::Number.number(3), publish_year: Faker::Stripe.year)
-    book_2 = Book.create(title: Faker::Book.title, authors: Faker::Book.author, pages: Faker::Number.number(3), publish_year: Faker::Stripe.year)
+    @book_1 = Book.create(title: Faker::Book.title, pages: Faker::Number.number(3), year_published: Faker::Stripe.year)
+    @book_2 = Book.create(title: Faker::Book.title, pages: Faker::Number.number(3), year_published: Faker::Stripe.year)
     end
     it 'displays all books' do
       visit books_path
 
-      expect(page).to have_content("Title: #{book_1.title}")
-      expect(page).to have_content("Author(s): #{book_1.authors}")
-      expect(page).to have_content("Pages: #{book_1.pages}")
-      expect(page).to have_content("Year Published: #{book_1.publish_year}")
-
-      expect(page).to have_content("Title: #{book_2.title}")
-      expect(page).to have_content("Author(s): #{book_2.authors}")
-      expect(page).to have_content("Pages: #{book_2.pages}")
-      expect(page).to have_content("Year Published: #{book_2.publish_year}")
+      expect(page).to have_content("Title: #{@book_1.title}")
+      expect(page).to have_content("Pages: #{@book_1.pages}")
+      expect(page).to have_content("Year Published: #{@book_1.year_published}")
     end
   end
 end
