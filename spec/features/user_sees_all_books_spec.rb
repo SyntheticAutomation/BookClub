d406aa50d2cb4507f2536030e4dcf6cbdee71e14
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-04 17:26:05 -0700

    Adds before(:each) block

--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe 'A visitor to our web app' do
   describe 'when I visit the books index page' do
+    before(:each) do
+    book_1 = Book.create(title: Faker::Book.title, authors: Faker::Book.author, pages: Faker::Number.number(3), publish_year: Faker::Stripe.year)
+    book_2 = Book.create(title: Faker::Book.title, authors: Faker::Book.author, pages: Faker::Number.number(3), publish_year: Faker::Stripe.year)
+    end
     it 'displays all books' do
-      book_1 = Book.create(title: Faker::Book.title, authors: Faker::Book.author, pages: Faker::Number.number(3), publish_year: Faker::Stripe.year)
-      book_2 = Book.create(title: Faker::Book.title, authors: Faker::Book.author, pages: Faker::Number.number(3), publish_year: Faker::Stripe.year)
-
-
       visit books_path
 
       expect(page).to have_content("Title: #{book_1.title}")
@@ -18,9 +18,6 @@ describe 'A visitor to our web app' do
       expect(page).to have_content("Author(s): #{book_2.authors}")
       expect(page).to have_content("Pages: #{book_2.pages}")
       expect(page).to have_content("Year Published: #{book_2.publish_year}")
-
-
-
     end
   end
 end
